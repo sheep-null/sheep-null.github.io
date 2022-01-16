d6db738dbe4b87d43ca848ef4fd133745326db1d
SHEEP NULL <sheep-null@users.noreply.github.com>
2022-01-17 03:34:11 +0900

    Updates

---
+++ component---src-pages-404-js-9ee1493961332e60d3c9.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_blog=self.webpackChunkgatsby_starter_blog||[]).push([[883],{9616:function(e,t,a){a.r(t);var n=a(7294),r=a(9308),c=a(194),l=a(4765);t.default=function(e){e.data;var t=e.location,a="404: Not Found";return n.createElement(c.Z,{location:t,title:a},n.createElement(r.Z,{variant:"h1"},a),n.createElement(r.Z,{my:2},"お探しのページは見つかりませんでした。",n.createElement(l.Z,{href:"/"},n.createElement(r.Z,{component:"span"},"TOPへ"))))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-9ee1493961332e60d3c9.js.map
