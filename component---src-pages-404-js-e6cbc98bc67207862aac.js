00739123315868a1bce3714fff471e82480b1496
koozaki <koozaki@R-MBP14UBN-0017.local>
2022-09-14 20:46:17 +0900

    Updates

---
+++ component---src-pages-404-js-e6cbc98bc67207862aac.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_blog=self.webpackChunkgatsby_starter_blog||[]).push([[883],{429:function(e,t,a){a.r(t);var n=a(7294),r=a(3854),c=a(6607),l=a(8596);t.default=function(e){e.data;var t=e.location,a="404: Not Found";return n.createElement(c.Z,{location:t,title:a},n.createElement(r.Z,{variant:"h1"},a),n.createElement(r.Z,{my:2},"お探しのページは見つかりませんでした。",n.createElement(l.Z,{href:"/"},n.createElement(r.Z,{component:"span"},"TOPへ"))))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-e6cbc98bc67207862aac.js.map
